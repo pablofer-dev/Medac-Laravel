de05c820c3d025aa583010a3745e6f6563d48337
Pablo <pfl0002@alu.medac.es>
2023-01-10 19:59:38 +0100

    fullcalendar

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -8,49 +8,19 @@ use Illuminate\Http\Request;
 
 class EventsController extends Controller
 {
-    public function index()
-    {
-        //
-    }
-
-    public function create()
-    {
-        //
-    }
-
     public function find()
     {
         $result = FechaHora::where('estado', 'no-reservada')->groupBy('fk_fecha')->get();
         $data = array();
         foreach ($result as $row) {
             $data[] = array(
-                'id' => $row->fk_fecha,
-                'title' => 'Reservar',
+                'id' => $row->id,
+                'title' => '',
                 'start' => $row->fk_fecha,
                 'end' => $row->fk_fecha,
-                
+
             );
         }
         return response()->json($data);
-    }
-
-    public function show($id)
-    {
-        //
-    }
-
-    public function edit($id)
-    {
-        //
-    }
-
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    public function destroy($id)
-    {
-        //
     }
 }
