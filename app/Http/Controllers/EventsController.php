71ddc86ad65568cfc14be3a68207a532a16d40bf
Pablo <pfl0002@alu.medac.es>
2023-01-25 20:11:59 +0100

    Disponible

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -18,7 +18,7 @@ class EventsController extends Controller
         foreach ($result as $key) {
             $data[] = array(
                 'id' => $key->id,
-                'title' => '',
+                'title' => 'Disponible',
                 'start' => $key->fk_fecha,
                 'end' => $key->fk_fecha,
             );
